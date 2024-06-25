9bc00ae3137e9d76a4797b9dab5a3bff3b19813a
Luis Gomero <lgomero94@gmail.com>
2024-06-25 00:51:07 -0500

    Add SHARED BARRIL Component

---
+++ src/app/domains/shared/shared.exports.ts
@@ -0,0 +1,5 @@
+import { CounterComponent } from './components/counter/counter.component';
+import { HeaderComponent } from './components/header/header.component';
+
+// Esto puede ser usado en los imports de los componentes standalone.
+export const SHARED_COMPONENTS = [CounterComponent, HeaderComponent];
