9f9a2f8c08ddabf3403333419e740bb6792c4ef7
Luis Gomero <lgomero94@gmail.com>
2024-07-13 00:38:37 -0500

    lifecycle

--- src/app/domains/shared/components/counter/counter.component.ts
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -45,6 +45,15 @@ export class CounterComponent
     }
   }
 
+  // ngOnChanges(changes: SimpleChanges) {
+  //   for (const inputName in changes) {
+  //     const inputValues = changes[inputName];
+  //     console.log(`Previous ${inputName} == ${inputValues.previousValue}`);
+  //     console.log(`Current ${inputName} == ${inputValues.currentValue}`);
+  //     console.log(`Is first ${inputName} change == ${inputValues.firstChange}`);
+  //   }
+  // }
+
   ngOnInit() {
     // after render
     // una vez
